92213ae1219b07d2467381aaeb207657379103e0
Mike <69970540+mikerb95@users.noreply.github.com>
2026-06-18 19:23:05 -0500

    Add useEffect import to ClientCard for enhanced state management

--- src/components/crm/components/ClientCard.jsx
+++ src/components/crm/components/ClientCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../CRM.module.css";
 import StageChip from "./StageChip.jsx";
 import { STAGES, SEGMENTS, KINDS, formatCOP, formatPesos, timeAgo, stageIndex } from "./constants.js";
